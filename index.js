16eebe2c098f65163b9a4a13524563b865084a3c
julia ferreira <juliaferreiraalvesdasilva@gmail.com>
2022-11-30 17:08:35 -0400

    trbalho api finalizado

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+
+const { produto } = require('./models')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+});
+
+app.get('/produtos', async function(req, res){
+    const produtos = await produto.findAll(req.body);
+    res.json(produtos)
+});
+
+app.listen(3000, () => {
+    console.log(`O servidor está rodando na porta 3000`)
+})
--- seeders/20221130181352-adicionar-produtos.js
+++ seeders/20221130181352-adicionar-produtos.js
@@ -3,15 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+      await queryInterface.bulkInsert('produtos', [
+        { nome: 'celular', preco: '600.00', descricao: 'celular apple', imagem: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617559'},
+        { nome: 'celular', preco: '600.00', descricao: 'celular samsung', imagem: 'https://images.samsung.com/is/image/samsung/p6pim/br/2202/gallery/br-galaxy-a53-5g-a536-sm-a536elbgzto-531544867?$650_519_PNG$'}
+      ], {});
   },
 
   async down (queryInterface, Sequelize) {
